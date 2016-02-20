e654cab584cfb627a0af523b1082c6852a375503
Mario Martinez <zenitram.oiram@gmail.com>
2016-02-20 20:02:30 +1100

    Update pair schema so that joins are possible

--- web/models/pair.ex
+++ web/models/pair.ex
@@ -17,45 +17,35 @@ defmodule WhatsBetter.Pair do
         table("pairs")
         |> insert(new_pair(pair_id, thing_1_id, thing_2_id, selected_thing))
         |> RethinkDB.run(db)
-      %{"thingOne" => %{"id" => ^selected_thing, "votes" => votes}} ->
+      %{"things" => things} ->
         table("pairs")
         |> get(pair_id)
-        |> update(%{"thingOne" => %{"votes" => votes + 1}})
-        |> RethinkDB.run(db)
-      %{"thingTwo" => %{"id" => ^selected_thing, "votes" => votes}} ->
-        table("pairs")
-        |> get(pair_id)
-        |> update(%{"thingTwo" => %{"votes" => votes + 1}})
+        |> update(%{"things" => update_votes(things, selected_thing) })
         |> RethinkDB.run(db)
     end
   end
 
-  def get_things_with_votes(pair_id, db \\ WhatsBetter.Database) do
-    #TODO: Is there a nicer way to merge the votes into things?
-    %Record{data: %{ "thingOne" => %{"id" => thing_1_id, "votes" => thing_1_votes},
-                     "thingTwo" => %{"id" => thing_2_id, "votes" => thing_2_votes}}
-                   } =
-      table("pairs")
-      |> get(pair_id)
-      |> RethinkDB.run(db)
-    %Collection{data: things_data} =
-      table("things")
-      |> get_all([thing_1_id, thing_2_id])
-      |> RethinkDB.run(db)
-  end
-
   defp new_pair(pair_id, thing_1_id, thing_2_id, selected_thing) do
     %{ "id" => pair_id,
-       "thingOne" => %{ "id"    => thing_1_id,
-                        "votes" => score(thing_1_id, selected_thing) },
-       "thingTwo" => %{ "id"    => thing_2_id,
-                        "votes" => score(thing_2_id, selected_thing) }
+       "things" => [%{ "id"    => thing_1_id,
+                       "votes" => score(thing_1_id, selected_thing) },
+                    %{ "id"    => thing_2_id,
+                       "votes" => score(thing_2_id, selected_thing) }]
     }
   end
 
   defp score(thing, selected_thing) do
     if selected_thing == thing, do: 1, else: 0
   end
+
+  defp update_votes(things, selected_thing) do
+    Enum.map(things, fn(thing) ->
+      case thing do
+        %{"id" => selected_thing} ->
+          Map.put(thing, "votes", thing["votes"] + 1)
+        end
+      end)
+    end
 
   # TODO: No need to use pair_id and use regular id if you take advanage of `has_fields`
   defp pair_id(things) do
