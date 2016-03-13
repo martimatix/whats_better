8dcd02a3360aa632a5a2f63cd4a93a34be41e792
Mario Martinez <zenitram.oiram@gmail.com>
2016-03-13 22:12:03 +1100

    Current pair and previous pair on same page

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -3,19 +3,32 @@ defmodule WhatsBetter.PageController do
 
   alias WhatsBetter.Thing
   alias WhatsBetter.Pair
+  alias WhatsBetter.Ranking
 
   plug :authenticate_user when action in [:vote]
 
   def index(conn, _params) do
     [thing_1, thing_2] = Thing.get_two_random
 
+    case _params["pair_id"] do
+      nil ->
+        previous_things = :no_previous_pair
+        best_things = Ranking.top
+        worst_things = Ranking.bottom
+      _ ->
+        previous_things = Pair.get_things_with_votes(_params["pair_id"])
+        [best_things, worst_things] = [nil, nil]
+    end
     render conn, "index.html",
       thing_1: thing_1,
-      thing_2: thing_2
+      thing_2: thing_2,
+      best_things: best_things,
+      worst_things: worst_things,
+      previous_things: previous_things
   end
 
   def vote(conn, params) do
     pair_id = Pair.vote(params["ballot"])
-    redirect(conn, to: pair_path(conn, :show, pair_id))
+    redirect(conn, to: page_path(conn, :index, pair_id: pair_id))
   end
 end
